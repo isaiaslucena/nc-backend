d20b8b76c9441a1c74b8dd033c015607e5756aad
isaiasneto <isaiasneto@gmail.com>
2022-09-07 23:42:43 -0300

    feat: added middleware and new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UserController } from './user/user.controller'
+import { UserModule } from './user/user.module'
+import { UserService } from './user/user.service'
+import { FirestoreModule } from './firestore/firestore.module'
+import { PreAuthMiddleware } from './auth/pre-auth.middleware'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, FirestoreModule],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(PreAuthMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    })
+  }
+}
